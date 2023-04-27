50c8e1a8987262982b9e2ce42d6e7bb73ca12c0d
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-04-27 17:38:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button , Form } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+      <div class="card shadow">
+        <div class="card-body">
+          <div class="text-center login-form ">
+           <Form>
+            <Form.Label>User</Form.Label>
+            <Form.Control type='text' placeholder='Username'></Form.Control>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Password'></Form.Control>
+           </Form>
+           <Button>Login</Button>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
